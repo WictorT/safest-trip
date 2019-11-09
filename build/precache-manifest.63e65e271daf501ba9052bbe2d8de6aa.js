1e8d67cfca67c2daffb7cd5e82860b70d41fd0cc
vtimoftii <victoritmoftii@yahoo.com>
2019-11-09 13:31:00 +0200

    build pushed

---
+++ build/precache-manifest.63e65e271daf501ba9052bbe2d8de6aa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c33a73667401c617c05b87516c1f4fd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "50fd3c194d7eb78a5922",
+    "url": "/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "94f920cf5d8a7e943794",
+    "url": "/static/css/main.543e0ef2.chunk.css"
+  },
+  {
+    "revision": "50fd3c194d7eb78a5922",
+    "url": "/static/js/2.8ab92b6f.chunk.js"
+  },
+  {
+    "revision": "94f920cf5d8a7e943794",
+    "url": "/static/js/main.3026a245.chunk.js"
+  },
+  {
+    "revision": "d653c3e1ff0c4fc20192",
+    "url": "/static/js/runtime-main.d01d61ca.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
